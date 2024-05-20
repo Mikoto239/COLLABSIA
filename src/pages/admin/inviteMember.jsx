ffd173ace3035558c34d9310af98fc8b44878f6f
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-20 19:41:03 +0800

    Update inviteMember.jsx

--- src/pages/admin/inviteMember.jsx
+++ src/pages/admin/inviteMember.jsx
@@ -48,7 +48,7 @@ const InviteMember = () => {
   };
 
   useEffect(() => {
-    fetch('https://collabsia.vercel.app/api/details', {
+    fetch('https://collabsiaserver.onrender.com/api/details', {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((res) => res.json())
@@ -62,7 +62,7 @@ const InviteMember = () => {
     const code = generateCode();
 
     try {
-      await axios.post('https://collabsia.vercel.app/api/invite', {
+      await axios.post('https://collabsiaserver.onrender.com/api/invite', {
         sender: email,
         recipient: recipient,
         subject: "Invitation From COT Department",
