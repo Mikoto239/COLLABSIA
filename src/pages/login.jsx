83be2a5db9d0d3252e11120dfedc2531761d3ed0
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-20 20:42:24 +0800

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { jwtDecode } from 'jwt-decode';
+
 import Header from '../components/header';
 import Footer from '../components/footer';
 
