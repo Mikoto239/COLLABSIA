8801bb5e7d0f6548865453ad0b90b161dd6d4a16
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-21 16:26:12 +0800

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -68,7 +68,7 @@ const Login = ({ history }) => {
   useEffect(() => {
     /*global google*/
     google.accounts.id.initialize({
-      client_id: '373547344231-uugg2iqm8p52tmq9iptiscn3905h1dlo.apps.googleusercontent.com', // Replace with your actual client ID
+      client_id: '373547344231-dbllh4hrgnn1m24sefu5plbfdcv4qurl.apps.googleusercontent.com', // Replace with your actual client ID
       callback: handleCallbackResponse,
     });
 
